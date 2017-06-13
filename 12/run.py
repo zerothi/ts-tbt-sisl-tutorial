939b6403093f697163b01c37fd44750747d65273
Nick Papior <nickpapior@gmail.com>
2017-06-13 12:26:52 +0200

    Updated for sisl > 0.8.4
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- 12/run.py
+++ 12/run.py
@@ -31,12 +31,12 @@ device = elec.repeat(5, axis=1).tile(4, axis=0)
 
 # Attach the chain on-top of an atom
 # First find an atom in the middle of the device
-idx = device.close(device.center(which='cell')*[1,1,0], dR=1.45)[1]
+idx = device.close(device.center(which='cell')*[1,1,0], R=1.45)[1]
 # Attach the chain at a distance of 2.25 along the third lattice vector
 device = device.attach(idx, chain, 0, dist=2.25, axis=2)
 # Add vacuum along chain, we really no not care how much vacuum, but it
 # is costly on memory.
-device = device.append(sisl.SuperCell([15]), axis=2)
+device = device.add_vacuum(15, axis=2)
 
 device.write('DEVICE.fdf')
 device.write('DEVICE.xyz')
