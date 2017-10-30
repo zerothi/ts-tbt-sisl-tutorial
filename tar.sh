9a6f83ca942c8e28be836584bba4be50ef27f506
Nick Papior <nickpapior@gmail.com>
2017-10-30 14:09:07 +0100

    added pivoting video
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ tar.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This file will tar all examples and required files for
+# the tutorial tar.gz file.
+
+_files=
+function add_file {
+    while [ $# -gt 0 ];
+    do
+	_files="$_files $1"
+	shift
+    done
+}
+
+# Add manuals
+add_file arch.make
+add_file siesta.pdf tbtrans.pdf
+add_file tselecs.sh
+
+# Add IPython books
+add_file tutorial.ipynb
+add_file */run.ipynb
+
+# Add RUN.fdf
+add_file */RUN.fdf
+add_file */RUN_ELEC.fdf
+add_file */RUN_ELEC_*.fdf
+
+# Add pseudo
+add_file C.psf
+add_file */C.psf
+add_file */*/C.psf
+
+# Add executables
+add_file */run.sh
+
+#echo These files will be added to the tar file:
+#echo "$(echo $_files | tr ' ' '\n')"
+
+tar cfz sisl-TBT-TS.tar.gz $_files
