55c5cda280c764688d14cf3e796102907052d6de
Nick Papior <nickpapior@gmail.com>
2020-05-29 12:35:14 +0200

    bug: fixed TB_06

--- TB_06/run.sh
+++ TB_06/run.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-# This script uses the sdata command to extract the
+# This script uses the sgeom command to extract the
 # bond-currents originating from all electrodes.
 # This is extracting the bond-currents at E = 0.1 eV
 for elec in X-1 X-2 Y-1 Y-2
 do
     echo Creating vector-currents for elec-$elec
-    sdata siesta.TBT.nc \
-    	  --vector vector_current elec-$elec 0.1 \
+    sgeom siesta.TBT.nc \
+	  --vector vector_current elec-$elec 0.1 \
 	  --out elec-${elec}_0.1.xsf
 done
